b9143d5e113fddcfa9fdc943c19141a8773eb6e7
Julio Soares Mendes <julio_soares_mendes@hotmail.com>
2023-03-15 05:04:45 -0300

    configurado a letra do star wars apenas para o header, para colocar o logo

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -4,12 +4,14 @@ import Navbar from "react-bootstrap/Navbar";
 import Image from "react-bootstrap/Image";
 import Logo from "../../assets/starwarsLogo.png";
 
+import "./header.css";
+
 export const Header = () => {
    return (
       <Navbar bg="dark" variant="dark">
-         <Container fluid>
-            <Navbar.Brand href="#home">
-               <Image src={Logo} fluid width="40" height="40" className="d-inline-block align-center" />
+         <Container>
+            <Navbar.Brand href="/">
+               <p className="logo">@</p>
             </Navbar.Brand>
             <Navbar.Collapse id="responsive-NavBar" className="justify-content-end">
                <Nav>
