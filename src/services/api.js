3f72b3e71cb263ac09683a36dcd966c8a4135049
Julio Soares Mendes <julio_soares_mendes@hotmail.com>
2023-03-14 23:52:42 -0300

    feat: criado arquivo service

---
+++ src/services/api.js
@@ -0,0 +1,19 @@
+const API_URL = "https://swapi.dev/api";
+
+async function fetchData(endpoint) {
+   const response = await fetch(`${API_URL}/${endpoint}`);
+
+   if (!response.ok) {
+      throw new Error(`Failed to fetch data from ${endpoint}`);
+   }
+
+   return response.json();
+}
+
+export async function getUserById(id) {
+   return fetchData(`people/${id}/`);
+}
+
+export async function getAllUsers() {
+   return fetchData(`people/`);
+}
