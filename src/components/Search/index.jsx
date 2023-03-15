2587ed75adb722e60ca0e591c5de6a344b56fbaa
Julio Soares Mendes <julio_soares_mendes@hotmail.com>
2023-03-15 12:33:54 -0300

    configurado função de busca por filme e personagens

--- src/components/Search/index.jsx
+++ src/components/Search/index.jsx
@@ -2,16 +2,21 @@ import InputGroup from "react-bootstrap/InputGroup";
 import DropdownButton from "react-bootstrap/DropdownButton";
 import Dropdown from "react-bootstrap/Dropdown";
 import Form from "react-bootstrap/Form";
+import { Button } from "react-bootstrap";
 
 export const Search = (props) => {
-   const { chanceValue, dropdownValue } = props;
+   const { changeValue, dropdownValue, value, inputOnchange, clearInput } = props;
+
    return (
       <InputGroup>
          <DropdownButton title={dropdownValue} id="dropdown-basic-button">
-            <Dropdown.Item onClick={(e) => chanceValue(e.target.textContent)}>Filme</Dropdown.Item>
-            <Dropdown.Item onClick={(e) => chanceValue(e.target.textContent)}>Personagem</Dropdown.Item>
+            <Dropdown.Item onClick={(e) => changeValue(e.target.textContent, "films")}>Filme</Dropdown.Item>
+            <Dropdown.Item onClick={(e) => changeValue(e.target.textContent, "people")}>Personagem</Dropdown.Item>
          </DropdownButton>
-         <Form.Control type="text" placeholder="Digite o nome do personagem/filme" />
+         <Form.Control id="Teste123" value={value} type="text" placeholder="Faça sua busca" onChange={(event) => inputOnchange(event)} />
+         <Button variant="primary" onClick={clearInput}>
+            Limpar
+         </Button>
       </InputGroup>
    );
 };
