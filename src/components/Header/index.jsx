fd595f62eeec97cfb1d0b2c4c2942f5dc266961e
Julio Soares Mendes <julio_soares_mendes@hotmail.com>
2023-03-16 01:44:45 -0300

    removido import sem uso

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,8 +1,6 @@
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
-import Image from "react-bootstrap/Image";
-import Logo from "../../assets/starwarsLogo.png";
 
 import "./header.css";
 
