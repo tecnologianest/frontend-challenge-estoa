273e6b653b5304e71dbc8d14cc277d95aea10418
Kayo Elias <contatodevkayo@gmail.com>
2023-03-27 14:25:40 -0300

    build : bootstrap config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
+import path, { resolve } from "path";
+
 export default defineConfig({
+  resolve: {
+    alias: {
+      "~bootstrap": path.resolve(__dirname, "node_modules/bootstrap"),
+      "@": resolve(__dirname, "src"),
+    },
+  },
   plugins: [react()],
-})
+});
