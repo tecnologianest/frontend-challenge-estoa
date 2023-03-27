40ce107e77edff64461b3869387e6e4a64a7b53c
Kayo Elias <contatodevkayo@gmail.com>
2023-03-27 14:25:55 -0300

    feat : routers to render interface

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,26 @@
+import "./styles/main.scss";
+import { HomePage } from "./pages/homePage";
+import { Menu } from "./components/Menu";
+
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { CharacterPage } from "./pages/characterPage";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <HomePage />,
+  },
+  {
+    path: "character/:characterID",
+    element: <CharacterPage />,
+  },
+]);
+
 export const App = () => {
-  return <h1>hello world</h1>;
+  return (
+    <>
+      <Menu />
+      <RouterProvider router={router} />
+    </>
+  );
 };
