fde1a793e4305048fdf751b1a7c179ac791b3b59
Lucas Renan <lucasrenan365@outlook.com>
2023-05-17 20:35:55 -0300

    [feat] - criado backend da aplicacao,  instalando dependencias e inicializando servidor com express.

---
+++ backend/src/server.ts
@@ -0,0 +1,19 @@
+import dotenv from "dotenv"
+dotenv.config()//necessario declarar o config antes de carregar nosso servido.
+
+import express from "express"
+import cors from "cors"
+
+//routes
+
+
+const app = express()
+
+const port = 3333
+app.use(cors())
+app.use(express.json())
+
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`)
+})
