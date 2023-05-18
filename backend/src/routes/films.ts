eb1b7db804e854df6d3254859491b10d8bc35caf
Lucas Renan <lucasrenan365@outlook.com>
2023-05-17 21:18:17 -0300

    [feat] - criada rota de filmes, criando opcao de filtro por filmes e os orenando por ordem de lancamento

---
+++ backend/src/routes/films.ts
@@ -0,0 +1,41 @@
+import dotenv from "dotenv";
+dotenv.config(); // É necessário declarar o config antes de carregar nosso servidor.
+
+import Router from "express";
+import { Request, Response } from "express";
+import axios from "axios";
+
+const filmsRouter = Router();
+
+const swapi = process.env.SWAPI_URL;
+
+filmsRouter.get("/films", async (req: Request, res: Response) => {
+  try {
+    const response = await axios.get(`${swapi}/films`);
+    let films = response.data.results;
+
+    const { search } = req.query; // Obtem os parametros de busca via query
+
+    if (search) {
+      // Se houver um termo de busca
+      const searchQuery = search.toString().toLowerCase();
+      films = films.filter((film: any) => {
+        // Filtra os filmes pelo nome, ignorando maiúsculas e minúsculas
+        return film.title.toLowerCase().includes(searchQuery);
+      });
+    }
+
+    // Ordena os filmes por data de lançamento
+    films.sort((a: any, b: any) => {
+      const dateA = new Date(a.release_date);
+      const dateB = new Date(b.release_date);
+      return dateA.getTime() - dateB.getTime();
+    });
+
+    res.json(films);
+  } catch (error) {
+    res.status(500).json({ error: "Failed to fetch films from SWAPI" });
+  }
+});
+
+export default filmsRouter;
