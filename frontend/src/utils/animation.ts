ea81a625644fdb764ffd1de7d0fcbb49b480a9ef
Lucas Renan <lucasrenan365@outlook.com>
2023-05-17 21:11:00 -0300

    [feat] - adicionando arquivos de animacao do framer-motion

---
+++ frontend/src/utils/animation.ts
@@ -0,0 +1,41 @@
+export const containerVariants = {
+    initial: {
+      opacity: 0 
+    },
+    animate: {
+      opacity: 1, 
+      transition: {
+        duration: 0.5, 
+        ease: "easeInOut" 
+      }
+    },
+    exit: {
+      opacity: 0, 
+      transition: {
+        duration: 0.5, 
+        ease: "easeInOut" 
+      }
+    }
+  };
+  
+  export const childVariants = {
+    initial: {
+      opacity: 0 
+    },
+    animate: {
+      opacity: 1, 
+      transition: {
+        duration: 0.5, 
+        ease: "easeInOut" 
+      }
+    },
+    exit: {
+      opacity: 0, 
+      transition: {
+        duration: 0.5,
+        ease: "easeInOut" 
+      }
+    }
+  };
+  
+  
