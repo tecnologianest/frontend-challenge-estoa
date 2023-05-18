6c2f62a98104bf31ce1648c734aaed9738d5658e
vinicius costa <vinicius.dsc95@gmail.com>
2023-05-17 23:40:43 -0300

    fix: now species list render correctly

--- src/context/CharactersContext.tsx
+++ src/context/CharactersContext.tsx
@@ -11,35 +11,46 @@ export const CharactersContext = createContext<{
 export function CharactersProvider({ children }: { children: ReactNode }) {
   const { data, isLoading } = useQuery({
     queryKey: ['characters'],
-    queryFn: getCharacters,
+    queryFn: getData,
   });
 
-  async function getCharacters() {
+  async function getData() {
     let characters: T.CharacterPropsResults = [];
 
     // set next page to fetch
-    let nextPage: string | null = 'https://swapi.dev/api/people/';
+    const nextPage: string | null = 'https://swapi.dev/api/people/';
 
+    // get only 10 results
+    const response: AxiosResponse<T.CharacterProps> =
+      await axios.get<T.CharacterProps>(nextPage);
+    characters = [...characters, ...response.data.results];
+
+    // get all results
+    // characters = (await axios.get<T.CharacterProps>(nextPage)).data
     // while nextPage is true, it does another fetch to return all results
-    while (nextPage) {
-      const response: AxiosResponse<T.CharacterProps> =
-        await axios.get<T.CharacterProps>(nextPage);
-      const { next, results } = response.data;
-      nextPage = next;
-      characters = [...characters, ...results];
-    }
+    // while (nextPage) {
+    //   const response: AxiosResponse<T.CharacterProps> =
+    //     await axios.get<T.CharacterProps>(nextPage);
+    //   const { next, results } = response.data;
+    //   nextPage = next;
+    //   characters = [...characters, ...results];
+    // }
 
     async function getSpecies() {
       await Promise.all(
         // loops over the data object
         characters.map(async (char, index) => {
+          const speciesList: string[] = [];
+
           await Promise.all(
             char.species.map(async (specie) => {
               // loops over the species list and fetch each specie
               const response = await axios.get<{ name: string }>(specie);
               const specieName = response.data.name;
+
+              speciesList.push(specieName);
               // updates the data object
-              characters[index].species = [specieName];
+              characters[index].species = [...speciesList];
             })
           );
         })
