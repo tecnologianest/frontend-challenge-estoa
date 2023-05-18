fb3873d9e2d42dc79bb50e19a2061924509797b1
vinicius costa <vinicius.dsc95@gmail.com>
2023-05-17 23:12:45 -0300

    feat: update species html loop

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,19 +1,32 @@
-import { useContext } from 'react'
-import { Loading } from '../components/Loading'
-import { CharactersContext } from '../context'
+import { useContext } from 'react';
+import { Loading } from '../components/Loading';
+import { CharactersContext } from '../context';
 
 export function Home() {
-  const { data, isLoading } = useContext(CharactersContext)
+  const { data, isLoading } = useContext(CharactersContext);
 
   if (isLoading) {
-    return <Loading />
+    return <Loading />;
   }
 
   return (
     <section>
-      {data?.map((character) => (
-        <div key={character.name}>{character.name}</div>
+      {data?.map(({ name, species, birth_year }) => (
+        <ul key={name}>
+          <li>name: {name}</li>
+          {species.length > 0 && (
+            <details open>
+              <summary>Species</summary>
+              <ul>
+                {species.map((specie) => (
+                  <li key={name + '-' + specie}>{specie}</li>
+                ))}
+              </ul>
+            </details>
+          )}
+          <li>birth year: {birth_year}</li>
+        </ul>
       ))}
     </section>
-  )
+  );
 }
