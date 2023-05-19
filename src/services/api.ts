29f59060b162905e6c8d49cc2ae78eedef581719
vinicius costa <vinicius.dsc95@gmail.com>
2023-05-19 04:04:06 -0300

    fix: betterapi callback

--- src/services/api.ts
+++ src/services/api.ts
@@ -3,25 +3,33 @@ import {
   CharacterProps,
   FilmProps,
   HomeworldProps,
+  PromiseProps,
   SpecieProps,
 } from '../types';
 
 const BASE_URL = 'https://swapi.dev/api/people';
 
-export const fetchCharacters = async (): Promise<CharacterProps[]> => {
-  const response = await axios.get(BASE_URL);
-  const characters: CharacterProps[] = response.data.results;
+export async function fetchCharacters() {
+  const characters: CharacterProps[] = [];
+  let nextPage: string | null = BASE_URL;
 
-  const speciesPromises = characters.map(async (character) => {
-    const species = await getSpecies(character.species);
-    return {
-      ...character,
-      species,
-    };
-  });
+  while (nextPage) {
+    const response = await axios.get<PromiseProps>(nextPage);
+    const data: PromiseProps = response.data;
+    nextPage = data.next;
+    characters.push(...data.results);
+  }
+
+  const speciesPromises = characters.map((character) =>
+    getSpecies(character.species)
+      .then((species) => ({ ...character, species }))
+      .catch(() => ({ ...character, species: 'Unknown' }))
+  );
+
   const charactersWithSpecies = await Promise.all(speciesPromises);
+
   return charactersWithSpecies;
-};
+}
 
 export async function getFilms(filmsList: string[]) {
   const list: string[] = [];
