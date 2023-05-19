6abba0cb9f5a51133dc4ba9e54fc09f97826980d
vinicius costa <vinicius.dsc95@gmail.com>
2023-05-18 23:04:31 -0300

    fix: lib moved to services

---
+++ src/services/api.ts
@@ -0,0 +1,55 @@
+import axios from 'axios';
+import {
+  CharacterProps,
+  FilmProps,
+  HomeworldProps,
+  SpecieProps,
+} from '../types';
+
+const BASE_URL = 'https://swapi.dev/api/people';
+
+export const fetchCharacters = async (): Promise<CharacterProps[]> => {
+  const response = await axios.get(BASE_URL);
+  const characters: CharacterProps[] = response.data.results;
+
+  const speciesPromises = characters.map(async (character) => {
+    const species = await getSpecies(character.species);
+    return {
+      ...character,
+      species,
+    };
+  });
+  const charactersWithSpecies = await Promise.all(speciesPromises);
+  return charactersWithSpecies;
+};
+
+export async function getFilms(filmsList: string[]) {
+  const list: string[] = [];
+
+  await Promise.all(
+    filmsList.map(async (film) => {
+      const { data } = await axios.get<FilmProps>(film);
+      list.push(data.title);
+    })
+  );
+
+  return list;
+}
+
+export async function getSpecies(speciesList: string[]) {
+  const list: string[] = [];
+
+  await Promise.all(
+    speciesList.map(async (specie) => {
+      const { data } = await axios.get<SpecieProps>(specie);
+      list.push(data.name);
+    })
+  );
+
+  return list;
+}
+
+export async function getHomeWorld(url: string) {
+  const { data } = await axios.get<HomeworldProps>(url);
+  return data.name;
+}
