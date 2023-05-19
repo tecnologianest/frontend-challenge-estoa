07a6a34a4ef1f4e7c7396fccb22366517c7bad96
marcossnikel <marcosnikeldev@gmail.com>
2023-05-18 22:38:03 -0300

    feat: background gradient

--- challenge-estoa/src/pages/index.tsx
+++ challenge-estoa/src/pages/index.tsx
@@ -1,9 +1,9 @@
-import CharacterCard from "../components/character-catalog";
+import CharacterCard from "../components/mainPage";
 
 export default function Home() {
   return (
-    <div className="">
-     <CharacterCard />
+    <div className="bg-gradient-to-b from-red-900 to-gray-500">
+      <CharacterCard />
     </div>
-  )
+  );
 }
