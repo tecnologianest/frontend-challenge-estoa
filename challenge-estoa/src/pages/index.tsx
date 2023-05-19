4e9e0772e9ffb6c4b31824caa5e8817b5af0a065
marcossnikel <marcosnikeldev@gmail.com>
2023-05-19 15:51:47 -0300

    fix: changing background

--- challenge-estoa/src/pages/index.tsx
+++ challenge-estoa/src/pages/index.tsx
@@ -2,7 +2,7 @@ import CharacterCard from "../components/mainPage";
 
 export default function Home() {
   return (
-    <div className="bg-gradient-to-b from-red-900 to-gray-500">
+    <div className="bg-starwars">
       <CharacterCard />
     </div>
   );
