ca48d534d3fd7ba5959004b988cf3d8bab4b2b12
rmdbarbosa <r_mdbarbosa@hotmail.com>
2023-08-30 18:16:18 -0300

    modal layout + character fetch render

--- desafio/src/app/components/character.js
+++ desafio/src/app/components/character.js
@@ -1,24 +1,76 @@
 "use client";
 
-import React from "react";
+import React, { useState } from "react";
 import { Card, CardHeader, CardBody, Image } from "@nextui-org/react";
+import {
+  Modal,
+  ModalContent,
+  ModalHeader,
+  ModalBody,
+  ModalFooter,
+  Button,
+  useDisclosure,
+} from "@nextui-org/react";
 
 export default function Character() {
+  const [modal, setModal] = useState(false);
+  const { isOpen, onOpen, onOpenChange } = useDisclosure();
+
   return (
-    <Card className="py-4 bg-slate-900 rounded-xl">
-      <CardHeader className="pb-0 pt-2 px-4 flex-col items-start">
-        <p className="text-tiny uppercase font-bold">Placeholder</p>
-        <small className="text-default-500">Placeholder</small>
-        <h4 className="font-bold text-large">Placeholder</h4>
-      </CardHeader>
-      <CardBody className="overflow-visible py-2">
-        <Image
-          alt="Card background"
-          className="object-cover rounded-xl"
-          src="https://i.pinimg.com/564x/97/53/e7/9753e7c3ab26a0f75246431cd1c2063b.jpg"
-          width={270}
-        />
-      </CardBody>
-    </Card>
+    <>
+      <Card className="py-4 bg-slate-900 rounded-xl">
+        <CardHeader className="pb-0 pt-2 px-4 flex-col items-start">
+          <p className="text-tiny uppercase font-bold">Luke Skywalker</p>
+          <small className="text-default-500">Birth Year: </small>
+          <h4 className="font-bold text-large cursor-pointer">Species: </h4>
+        </CardHeader>
+        <CardBody className="overflow-visible py-2">
+          <Image
+            alt="Luke Skywalker"
+            className="object-cover rounded-xl"
+            src="https://i.pinimg.com/564x/97/53/e7/9753e7c3ab26a0f75246431cd1c2063b.jpg"
+            width={270}
+          />
+        </CardBody>
+        <Button onPress={onOpen}>More info</Button>
+      </Card>
+      <Modal
+        className="bg-slate-900 rounded-xl"
+        isOpen={isOpen}
+        onOpenChange={onOpenChange}
+      >
+        <ModalContent>
+          {(onClose) => (
+            <>
+              <ModalHeader className="flex flex-col gap-1 text-center text-slate-400">
+                Luke Skywalker
+              </ModalHeader>
+              <ModalBody>
+                <img
+                  className="rounded-xl"
+                  src="https://i.pinimg.com/564x/97/53/e7/9753e7c3ab26a0f75246431cd1c2063b.jpg"
+                />
+                <div className="flex">
+                  <ul className="space-y-1">
+                    <li>Name: Placeholder bla bla</li>
+                    <li>Birth Year: </li>
+                    <li>Eye Color: </li>
+                    <li>Gender: </li>
+                    <li>Hair Color: </li>
+                    <li>Height: </li>
+                    <li>Mass: </li>
+                    <li>Skin Color: </li>
+                    <li>Homeworld: </li>
+                    <li>Films: </li>
+                    <li>Species: </li>
+                  </ul>
+                </div>
+              </ModalBody>
+              <ModalFooter></ModalFooter>
+            </>
+          )}
+        </ModalContent>
+      </Modal>
+    </>
   );
 }
--- desafio/src/app/page.js
+++ desafio/src/app/page.js
@@ -1,13 +1,29 @@
 "use client";
 
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 import Character from "./components/character";
 
 export default function Home() {
+  const [characters, setCharacters] = useState([]);
+
+  useEffect(() => {
+    async function getCharacters() {
+      const response = await fetch("https://swapi.dev/api/people/");
+      const data = await response.json();
+      setCharacters(data.results);
+    }
+
+    getCharacters();
+  }, []);
+
+  const charactersElements = characters.map((character, index) => {
+    return <Character />;
+  });
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <Character />
+    <main className="flex min-h-screen flex-row flex-wrap items-center p-24 gap-8">
+      {charactersElements}
     </main>
   );
 }
