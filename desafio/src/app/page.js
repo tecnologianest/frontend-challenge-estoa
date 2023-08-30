09ad00054654b60a5c9006db36f4c62b9982ba38
rmdbarbosa <r_mdbarbosa@hotmail.com>
2023-08-30 15:58:45 -0300

    first commit / initial config

---
+++ desafio/src/app/page.js
@@ -0,0 +1,9 @@
+import { Button } from "@nextui-org/button";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <Button>Press me</Button>
+    </main>
+  );
+}
