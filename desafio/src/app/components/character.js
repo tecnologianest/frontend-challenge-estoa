967a16be147017f48799162fc1b571d7384d47fd
rmdbarbosa <r_mdbarbosa@hotmail.com>
2023-08-30 20:08:49 -0300

    added movies for each character

--- desafio/src/app/components/character.js
+++ desafio/src/app/components/character.js
@@ -1,5 +1,6 @@
 "use client";
 
+import { useState, useEffect } from "react";
 import { Card, CardHeader, CardBody, Image } from "@nextui-org/react";
 import {
   Modal,
@@ -13,6 +14,23 @@ import {
 
 export default function Character(props) {
   const { isOpen, onOpen, onOpenChange } = useDisclosure();
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    async function getMovies() {
+      const response = await fetch("https://swapi.dev/api/films/");
+      const data = await response.json();
+      setMovies(data.results);
+    }
+
+    getMovies();
+  }, []);
+
+  const sortedMovies = movies?.sort((a, b) =>
+    a.episode_id > b.episode_id ? 1 : -1
+  );
+
+  console.log(props);
 
   return (
     <>
@@ -23,9 +41,7 @@ export default function Character(props) {
             Birth Year: {props.birth_year}
           </small>
           {props.species.length > 0 && (
-            <h4 className="font-bold text-large cursor-pointer">
-              Species: Droid
-            </h4>
+            <small className="text-default-500">Species: Droid</small>
           )}
         </CardHeader>
         <CardBody className="overflow-visible py-2">
@@ -33,7 +49,7 @@ export default function Character(props) {
             alt={props.name}
             className="object-cover rounded-xl"
             src={`${props.name}.jpg`}
-            width={250}
+            width={270}
           />
         </CardBody>
         <Button onPress={onOpen}>More info</Button>
@@ -61,7 +77,27 @@ export default function Character(props) {
                     <li>Mass: {props.mass}</li>
                     <li>Skin Color: {props.skin_color}</li>
                     <li>Homeworld: {props.homeworld}</li>
-                    <li>Films: {props.films}</li>
+                    <li className="movies__li">
+                      Movies:
+                      {props.films[0].substr(28, 1) === "1" && (
+                        <li>{sortedMovies[0]?.title}</li>
+                      )}
+                      {props.films[1]?.substr(28, 1) === "2" && (
+                        <li>{sortedMovies[1]?.title}</li>
+                      )}
+                      {props.films[2]?.substr(28, 1) === "3" && (
+                        <li>{sortedMovies[2]?.title}</li>
+                      )}
+                      {props.films[3]?.substr(28, 1) === "4" && (
+                        <li>{sortedMovies[3]?.title}</li>
+                      )}
+                      {props.films[4]?.substr(28, 1) === "5" && (
+                        <li>{sortedMovies[4]?.title}</li>
+                      )}
+                      {props.films[5]?.substr(28, 1) === "6" && (
+                        <li>{sortedMovies[5]?.title}</li>
+                      )}
+                    </li>
                     {props.species.length > 0 && <li>Species: Droid</li>}
                   </ul>
                 </div>
