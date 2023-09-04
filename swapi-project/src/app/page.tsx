f126f13f40f1c91f32733a54bae771802002b18c
Cléber Severo <clebersevero1998@gmail.com>
2023-09-04 16:29:32 -0300

    feat: general page paddings matching

--- swapi-project/src/app/page.tsx
+++ swapi-project/src/app/page.tsx
@@ -1,5 +1,7 @@
 export default function Home() {
-  return <main className="bg-[#141318] text-blue-400 min-h-screen">
-    hello
-  </main>;
+  return (
+    <main className="bg-[#141318] text- min-h-screen px-32 py-4">
+      <h1 className="font-semibold text-xl text-sky-400 mt-4 border-b-2 border-sky-400 inline-block pr-12">Star Wars characters list</h1>
+    </main>
+  );
 }
--- swapi-project/src/components/Header/index.tsx
+++ swapi-project/src/components/Header/index.tsx
@@ -4,17 +4,18 @@ import React from "react";
 export default function Header() {
     
   return (
-    <header className="w-full flex gap-8 items-center p-4 bg-[#000000]">
+    <header className="w-full flex gap-8 items-center px-32 py-4 bg-[#000000]">
       <img src="assets/images/main-Logo.png" alt="logo" className="w-24"/>
       <nav>
-        <ul className="flex gap-4 font-semibold text-slate-100">
-          <li className="hover:cursor-pointer hover:text-slate-300">Home</li>
-          <li className="hover:cursor-pointer hover:text-slate-300">
+        <ul className="flex gap-4 font-semibold text-slate-100 select-none">
+          <li className="hover:cursor-pointer hover:text-sky-400">Home</li>
+          <li className="hover:cursor-pointer hover:text-sky-400">
             Favorites
           </li>
         </ul>
 
       </nav>
+        
     </header>
   );
 }
