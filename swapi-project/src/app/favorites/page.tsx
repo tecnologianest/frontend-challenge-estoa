78833442a4db6e953ef7d0346ccffb2f4f4359ed
Cléber Severo <clebersevero1998@gmail.com>
2023-09-06 10:53:05 -0300

    feat: favorite page text

--- swapi-project/src/app/favorites/page.tsx
+++ swapi-project/src/app/favorites/page.tsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import React from "react";
 
 function page() {
   return (
-    <div>This is the favorites page</div>
-  )
+    <div className="flex flex-col items-center pt-20">
+      <h1 className="text-slate-100 fons-bold">
+        Favorites page is a future Feature :)
+      </h1>
+    </div>
+  );
 }
 
-export default page
+export default page;
