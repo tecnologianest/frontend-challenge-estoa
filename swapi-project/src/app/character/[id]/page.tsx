a94bfb958bbc65365135a0d72793d509b23d85ff
Cléber Severo <clebersevero1998@gmail.com>
2023-09-06 10:10:43 -0300

    feat: movies list loading

--- swapi-project/src/app/character/[id]/page.tsx
+++ swapi-project/src/app/character/[id]/page.tsx
@@ -3,12 +3,9 @@ import CharacterInfo from "@/components/CharacterInfo";
 import Homeworld from "@/components/Homeworld";
 import Specie from "@/components/Specie";
 import { useAppDispatch, useAppSelector } from "@/hooks/redux-hooks";
-import {
-  clearSelectedCharacter,
-  fetchCharacterDetails,
-} from "@/redux/reducers/characters";
+import { clearSelectedCharacter,fetchCharacterDetails } from "@/redux/reducers/characters";
 import { fetchFilms } from "@/redux/reducers/films";
-import { Chip } from "@nextui-org/react";
+import { Chip, Spinner } from "@nextui-org/react";
 import React, { useEffect, useState } from "react";
 
 interface PageProps {
@@ -19,6 +16,7 @@ interface PageProps {
 
 function page({ params }: PageProps) {
   const dispatch = useAppDispatch();
+  const loadingFilm = useAppSelector( state => state.films.loading)
   const characterDetails = useAppSelector(
     (state) => state.characters.selectedCharacter
   );
@@ -27,7 +25,6 @@ function page({ params }: PageProps) {
   );
 
   const [moviesList, setMoviesList] = useState<any>([]);
-  console.log(characterFilms);
 
   useEffect(() => {
     dispatch(fetchCharacterDetails(params.id));
@@ -49,7 +46,10 @@ function page({ params }: PageProps) {
           if (film == item.url) {
             console.log("same");
             console.log(item.title);
-            setMoviesList((prevMovieList:string[]) => [...prevMovieList, item.title]);
+            setMoviesList((prevMovieList: string[]) => [
+              ...prevMovieList,
+              item.title,
+            ]);
           }
         });
       });
@@ -61,20 +61,28 @@ function page({ params }: PageProps) {
       className="flex justify-center items-center bg-[#141318]
                   box-border overflow-hidden pt-8 md:pt-20"
     >
-      <div className="flex flex-col  md:flex-row gap-4 bg-[#23222b] px-8 md:px-32 py-4 rounded w-[85%] md:w-[60%]">
-        <div className="flex flex-col items-end gap-4 w-[50%]">
-          <h1 className="mb-2 font-bold text-lg text-yellow-500 w-fit">
+      <div className="flex flex-col  md:flex-row gap-8 bg-[#23222b] px-8 md:px-32 py-4 md:py-8 rounded w-[85%] md:w-[60%]">
+        <div className="flex flex-col gap-2 md:items-end md:w-[50%]">
+          <h1 className="mb-4 font-bold text-lg text-yellow-500 w-fit">
             {characterDetails.name}
           </h1>
 
-          {moviesList.map((movie: string, index: number) => (
-            <Chip size="sm" key={index} className="text-slate-800">
-              {movie}
-            </Chip>
-          ))}
+          {loadingFilm && (
+            <Spinner
+              labelColor="warning"
+              color="default"
+              className="mt-16 ml-auto"
+              size="sm"
+            />
+          )}
+          {!loadingFilm &&
+            moviesList.map((movie: string, index: number) => (
+              <Chip size="sm" key={index} className="text-slate-800">
+                {movie}
+              </Chip>
+            ))}
         </div>
         <aside className="flex flex-col gap-2">
-          <CharacterInfo text="Nome" info={characterDetails.name} />
           <CharacterInfo text="Nascimento" info={characterDetails.birth_year} />
           <CharacterInfo
             text="Cor dos olhos"
